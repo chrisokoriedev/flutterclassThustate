5f234f1a62c09307d1e9cea3389c483060d2fe46
chrisokoriedev <okoriec01@gmail.com>
2024-09-19 14:11:14 +0100

    update

---
+++ ifstatement.dart
@@ -0,0 +1,102 @@
+// void main() {
+//   int number = 100;
+//   do {
+//     number++;
+//     print(number);
+//   } while (number < 1000000);
+// }
+// void main() {
+//   int number = 0;
+
+//   while (number < 11) {
+//     if (number % 3 == 1) {
+//       print(number);
+//     }
+//     number++;
+//   }
+// }
+// void main() {
+//   for (var current = 0; current <=3; current++) {
+//     print('current number $current');
+//   }
+// }
+// void main() {
+//   int total = 10;
+//   for (var x = 3; x <= 10; x++) {
+//     total += x;
+//   print('total product $total');
+//   }
+// }
+
+// void main() {
+//   String language = "Thustate";
+//   for (int i = 0; i < language.length; i++) {
+//     print(language[i]);
+//   }
+// }
+// void main() {
+//   int number = 0;
+//   do {
+//     if (number % 2 == 1) {
+//       print(number);
+//     }
+//     number++;
+//   } while (number < 10);
+// }
+// void main() {
+//   int dayNumber = 4;
+//   String dayName = "";
+
+//   switch (dayNumber) {
+//     case 1:
+//       dayName = "Sunday";
+//       break;
+//     case 2:
+//       dayName = "Monday";
+//       break;
+//     case 3:
+//       dayName = "Tuesday";
+//      if(dayName=="Tuesday"){
+//       print('Let go to work');
+//      }
+//       break;
+//     case 4:
+//       dayName = "Wednesday";
+//       break;
+//     case 5:
+//       dayName = "Thursday";
+//       break;
+//     case 6:
+//       dayName = "Friday";
+//       break;
+//     case 7:
+//       dayName = "Saturday";
+//     // default:
+//     //   dayName = "Invalid day";
+//   }
+//   print(dayName);
+// }
+
+
+void main() {
+  String dayName = "Monday";
+  String day = "";
+
+  switch (dayName) {
+    case "Monday":
+    case "Tuesday":
+    case "Wednesday":
+    case "Thursday":
+    case "Friday":
+      day = "Weekday";
+      break;
+    case "Saturday":
+    case "Sunday":
+      day = "Weekend";
+      break;
+    default:
+      day = "Invalid";
+  }
+
+  print(day);
+}
