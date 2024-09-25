7c95e01791cc57ebc9ba0c3e913ec6afec7ab896
chrisokoriedev <okoriec01@gmail.com>
2024-09-25 12:43:52 +0100

    "Refactor function.dart: comment out old main functions and ageAccount function, add new connect function with optional and required parameters, and a new main function to call connect."

--- function.dart
+++ function.dart
@@ -5,17 +5,52 @@
 // void greet(String name) {
 //   print('Hello, $name!');
 // }
-void main() {
-  String name = 'John';
-  int age = 23;
-  var newage = ageAccount(name, age);
-  print(newage);
+// void main() {
+//   String name = 'John';
+//   int age = 23;
+//   var newage = ageAccount(name, age);
+//   print(newage);
+// }
+
+// //Function Signature
+// ageAccount(String name, int age) {
+//   //Function body
+//   print('Hello, $name');
+//   print('You are $age years old');
+//   return ' $name is $age';
+// }
+
+// void main() {
+//   print(greet('John'));
+//   print(greet('thustate','Zaddy'));
+// }
+
+// String greet(String name, [String title = 'Mr']) {
+//   if (title.isEmpty) {
+//     return 'Hello $name';
+//   }
+//   return 'Hello $title $name!';
+// }
+
+// void main() {
+//   print(greet(title: '', name: 'thustate'));
+// }
+
+// String greet({String name = '', String title = ''}) {
+//   if (title.isEmpty) {
+//     return 'Hello $name!';
+//   }
+//   return 'Hello $title $name!';
+// }
+void connect(
+  String host, {
+  int port = 3306,
+  required String user,
+  required String password,
+}) {
+  print('Connecting to $host on $port using $user/$password...');
 }
 
-//Function Signature
-ageAccount(String name, int age) {
-  //Function body
-  print('Hello, $name');
-  print('You are $age years old');
-  return ' $name is $age';
+void main() {
+  connect('localhost', user: 'root', password: 'password');
 }
