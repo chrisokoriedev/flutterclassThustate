37090f926ad03c8df8eb113ae33816162c7e6732
chrisokoriedev <okoriec01@gmail.com>
2024-09-27 12:42:27 +0100

    "Added empty line to launch.json"

---
+++ class_teaching.dart
@@ -0,0 +1,41 @@
+// void main() {
+//   TeachingClass()
+//     ..addnewStudent(3, 'Thustate', 'Thustate is a Good boy')
+//     ..showdetails();
+// }
+
+// class TeachingClass {
+//   int id = 0;
+//   String name = '';
+//   String description = '';
+//   void addnewStudent(int id, String name, String description) {
+//     this.id = id;
+//     this.name = name;
+//     this.description = description;
+//   }
+
+//   void showdetails() => print('Students Details: $id $name $description');
+// }
+
+// class Point {
+//   int x = 0;
+//   int y = 0;
+
+//   Point(int x, int y) {
+//     this.x = x;
+//     this.y = y;
+//   }
+
+//   void reset() {
+//     this.x = 0;
+//     this.y = 0;
+//   }
+
+//   void show() {
+//     print('Point($x,$y)');
+//   }
+// }
+
+// void main() {
+//  Point(0, 10).show();
+// }
