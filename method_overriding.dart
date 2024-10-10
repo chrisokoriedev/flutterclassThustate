f85e7f2788b458385b73137b147682cb5ea8a199
chrisokoriedev <okoriec01@gmail.com>
2024-10-10 12:13:46 +0100

    update

---
+++ method_overriding.dart
@@ -0,0 +1,55 @@
+class BankAccount {
+  double _balance = 0;
+
+  BankAccount({double balance = 0}) : _balance = balance;
+
+  double get balance => _balance;
+
+  deposit(double amount) {
+    _balance += amount;
+  }
+
+  bool withdraw(double amount) {
+    if (amount <= _balance) {
+      _balance -= amount;
+      return true;
+    }
+    return false;
+  }
+
+  @override
+  String toString() {
+    return 'The balance is $balance USD.';
+  }
+}
+
+class SavingAccount extends BankAccount {
+  double _interestRate = 0;
+
+  SavingAccount({double balance = 0, double interestRate = 0})
+      : _interestRate = interestRate,
+        super(balance: balance);
+
+  double get interestRate => _interestRate;
+
+  set interestRate(double value) {
+    if (value > 0) {
+      _interestRate = value;
+    }
+  }
+
+  addInterest() {
+    double interest = _balance * _interestRate;
+    this._balance += interest;
+  }
+
+  @override
+  String toString() {
+    return super.toString() + 'The interest rate is ${interestRate}.';
+  }
+}
+
+void main() {
+  var account = SavingAccount(balance: 1000, interestRate: 10);
+  print(account);
+}
