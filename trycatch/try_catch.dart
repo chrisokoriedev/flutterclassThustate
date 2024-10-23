aec6d75e5b25ee1d3daef1133923f21f0d974713
chrisokoriedev <okoriec01@gmail.com>
2024-10-23 13:54:52 +0100

    "Modified try-catch block in main function, added new variable and list, changed print statements and error handling."

--- trycatch/try_catch.dart
+++ trycatch/try_catch.dart
@@ -1,11 +1,17 @@
 void main() {
+  String message = "Hello";
+  List messages = ["Hello", "World","how","are","you"];
+
   try {
-    String message = "Hello";
-    print("The character at the position 5 is ${message[5]}.");
-    print('Bye!');
+    // print("The character at the position 5 is ${message[5]}.");
+    print("The character at the position 5 is ${messages[0]}.");
+  } on RangeError {
+    // print('The valid range for the index is [0..${message.length - 1}].');
   } catch (e) {
-    print('The exception message is: $e');
+    print(e);
   }
+
+  print('Bye!');
 }
 ///The exception message is:
 /// RangeError (index): Invalid value: Not in inclusive range 0..4: 5
