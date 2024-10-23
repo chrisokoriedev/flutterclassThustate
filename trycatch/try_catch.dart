cfe169c1f7c4d63a794dcc756cd97f970a1cf9ab
chrisokoriedev <okoriec01@gmail.com>
2024-10-23 13:45:49 +0100

    Added try-catch block to handle exception in main function.

--- trycatch/try_catch.dart
+++ trycatch/try_catch.dart
@@ -1,5 +1,11 @@
 void main() {
-  String message = "Hello";
-  print("The character at the position 5 is ${message[5]}.");
-  print('Bye!');
+  try {
+    String message = "Hello";
+    print("The character at the position 5 is ${message[5]}.");
+    print('Bye!');
+  } catch (e) {
+    print('The exception message is: $e');
+  }
 }
+///The exception message is:
+/// RangeError (index): Invalid value: Not in inclusive range 0..4: 5
